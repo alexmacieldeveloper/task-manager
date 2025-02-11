ed39f17c7d87873c8e05702539ced0a36bc35308
Álex Maciel <aweasley.gina@gmail.com>
2025-02-11 20:32:37 -0300

    creating create task function

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+  listTasks: any[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.listTasks = [
+      { id: 0, name: 'Lavar o carro', done: false },
+      { id: 1, name: 'Ir ao mercado', done: true },
+      { id: 2, name: 'Lavar roupa', done: true },
+    ]
+  }
+
+  addTask(nameTask: string) {
+    if (nameTask.trim().length == 0) {
+      return;
+    }
+
+    const taskFound = this.listTasks.find(item => item.name.toLowerCase() == nameTask.toLowerCase());
+
+    if (!taskFound) {
+      this.listTasks.push({ id: this.listTasks.length, name: nameTask, done: false })
+    }
+  }
+
+}
