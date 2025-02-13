0fb23c8b6be3285dbf7f89a90f47e0e84ba85a29
Álex Maciel <aweasley.gina@gmail.com>
2025-02-12 23:32:32 -0300

    creating save list task method

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -6,16 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
+  TASK_KEY =  'task_key'
   listTasks: any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    this.listTasks = [
-      { id: 0, name: 'Lavar o carro', done: false },
-      { id: 1, name: 'Ir ao mercado', done: true },
-      { id: 2, name: 'Lavar roupa', done: true },
-    ]
+    const tasks = localStorage.getItem(this.TASK_KEY)
+
+    if (tasks) {
+      this.listTasks = JSON.parse(tasks)
+    }
   }
 
   addTask(nameTask: string) {
@@ -28,18 +29,27 @@ export class TaskComponent implements OnInit {
     if (!taskFound) {
       this.listTasks.push({ id: this.listTasks.length, name: nameTask, done: false })
     }
+
+    this.saveList()
   }
 
   deleteTask(idTask: number) {
     this.listTasks = this.listTasks.filter(item => (item.id != idTask))
+    this.saveList()
   }
 
-  doneTask(idTask: numberd) {
+  doneTask(idTask: number) {
     const taskFound = this.listTasks.find(item => (item.id == idTask))
 
     if (taskFound) {
       taskFound.done = !taskFound.done
     }
+
+    this.saveList()
+  }
+
+  private saveList() {
+    localStorage.setItem(this.TASK_KEY,JSON.stringify(this.listTasks))
   }
 
 }
